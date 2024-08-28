fc4b3c7938a19076bf912f0255ee864bbeeb916b
Herlan <herlan.n9d@gmail.com>
2024-08-28 12:07:27 +0700

    update scenario transfer feature

--- src/test/java/AutomationMobileJangkau/StepDefinitions/HomeSteps.java
+++ src/test/java/AutomationMobileJangkau/StepDefinitions/HomeSteps.java
@@ -3,9 +3,15 @@ package AutomationMobileJangkau.StepDefinitions;
 import AutomationMobileJangkau.BaseTest;
 import AutomationMobileJangkau.page.HomePage;
 import AutomationMobileJangkau.page.LoginPage;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.testng.AssertJUnit.assertTrue;
 
@@ -25,6 +31,7 @@ public class HomeSteps extends BaseTest {
         homepage.validateHomePage();
     }
 
+    //check balance
     @Given("user is login with valid credentials")
     public void userIsLoginWithValidCredentials() {
     }
@@ -34,8 +41,46 @@ public class HomeSteps extends BaseTest {
         homepage.remainBalance();
     }
 
+    //Transfer
     @Given("User is on the Transfer Antar BCA screen")
     public void userIsOnTheTransferAntarBCAScreen() {
+        homepage.clickTransferButton();
+        homepage.clickNewInputButton();
+        homepage.validateTransferPage();
+    }
+
+    @When("User fills the form with the following details:")
+    public void userFillsTheFormWithTheFollowingDetails(DataTable dataTable) {
+        List<Map<String, String>> formData = dataTable.asMaps(String.class, String.class);
+
+        for (Map<String, String> data : formData) {
+            String field = data.get("Field");
+            String value = data.get("Value");
+
+            switch(field) {
+                case "Nomor Rekening Tujuan":
+                    homepage.inputAccountNumberDestination(value);
+                    break;
+                case "Nominal Transfer":
+                    homepage.inputTransferBalance(value);
+                    break;
+                case "Catatan":
+                    homepage.inputTransferNotes(value);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unknown field: " + field);
+            }
+        }
+    }
+
+    @And("User clicks on Lanjutkan")
+    public void userClicksOnLanjutkan() {
+        homepage.clickNextTransferButton();
+    }
+
+    @Then("User should see the confirmation screen")
+    public void userShouldSeeTheConfirmationScreen() {
+
     }
 
 
--- src/test/java/AutomationMobileJangkau/page/HomePage.java
+++ src/test/java/AutomationMobileJangkau/page/HomePage.java
@@ -20,23 +20,59 @@ public class HomePage extends BaseTest {
     //transfer feature locator
     By transferButton = AppiumBy.accessibilityId("Tombol Transfer antar BCA");
     By newInputButton = AppiumBy.accessibilityId("Tombol Transfer Antar BCA dengan tujuan baru");
+    By transferBanner = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvTitlePage']");
+    By accountNumberSource = By.xpath("//androidx.cardview.widget.CardView[@resource-id='com.example.jangkau:id/cardRekeningSumber']");
     By accountNumberDestination = AppiumBy.accessibilityId("Textfield input nomor rekening tujuan");
     By transferBalance = AppiumBy.accessibilityId("Textfield input nominal transfer");
     By transferNotes = AppiumBy.accessibilityId("Textfield input catatan");
     By nextTransferButton = By.xpath("//android.widget.Button[@resource-id='com.example.jangkau:id/btnNext']");
     By confirmTransferButton = By.xpath("//android.widget.Button[@resource-id='com.example.jangkau:id/btnNext']");
     By berandaButton = AppiumBy.accessibilityId("tombol beranda");
+    //validate transfer
+    By checkAccountNameDestination = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvName']");
+    By checkAccountNumber = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvRekening']");
+    By checkAmountTransfer = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvNominal']");
+    By checkTotalAmount = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvTransfer']");
+    By checkAccountSource = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvLabelRekening']");
     //PIN locator
     By pinNo1 = AppiumBy.accessibilityId("Number 1");
     By pinNo4 = AppiumBy.accessibilityId("Number 4");
     By pinNo2 = AppiumBy.accessibilityId("Number 2");
     By pinNo6 = AppiumBy.accessibilityId("Number 6");
+    //validate transfer success
+    By checkSuccessIcon = AppiumBy.accessibilityId("Ilustrasi Transaksi Berhasil");
+    By checkIDTransaction = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvTransactionId']");
+    By accountRecipient = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvRekening']");
+    By checkAmount = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvNominal']");
+    By checkTransactionTime = By.xpath("//android.widget.TextView[@resource-id='com.example.jangkau:id/tvDate']");
+
 
     //Method Action
     public void validateHomePage(){
         wait.until(ExpectedConditions.presenceOfElementLocated(userNameBanner));
         wait.until(ExpectedConditions.presenceOfElementLocated(remainingBalace));
         wait.until(ExpectedConditions.presenceOfElementLocated(accountNumber));
+    }
+
+    public void validateTransferPage(){
+        wait.until(ExpectedConditions.presenceOfElementLocated(transferBanner));
+        wait.until(ExpectedConditions.presenceOfElementLocated(accountNumberSource));
+    }
+
+    public void validateBeforeTransfer(){
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkAccountNameDestination));
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkAccountNumber));
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkAmountTransfer));
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkTotalAmount));
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkAccountSource));
+    }
+
+    public void validateTransferSuccess(){
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkSuccessIcon));
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkIDTransaction));
+        wait.until(ExpectedConditions.presenceOfElementLocated(accountRecipient));
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkAmount));
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkTransactionTime));
     }
 
     public void remainBalance() {
@@ -73,5 +109,12 @@ public class HomePage extends BaseTest {
 
     public void clickBerandaButton(){
         wait.until(ExpectedConditions.visibilityOfElementLocated(berandaButton)).click();
+    }
+
+    public void clickPinButton(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(pinNo1)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(pinNo4)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(pinNo2)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(pinNo6)).click();
     }
 }
